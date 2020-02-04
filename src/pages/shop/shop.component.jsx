25eae5a8595092a4e9aeaacaf942f8958a09035d
Hugo Couto <hugo.couto@webhub.com.br>
2020-02-04 17:24:49 -0300

    196. useEffect In Our App

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -7,30 +7,24 @@ import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
 import CollectionPageContainer from '../collection/collection.container';
 
-class ShopPage extends React.Component {
-  componentDidMount() {
-    const { fetchCollectionsStart } = this.props;
-
-    fetchCollectionsStart();
-  }
-
-  render() {
-    const { match } = this.props;
+const ShopPage = ({ fetchCollectionsStart, match }) => {
+    useEffect(() => {
+        fetchCollectionsStart();
+    }, [fetchCollectionsStart]);
 
     return (
-      <div className='shop-page'>
+        <div className='shop-page'>
         <Route
-          exact
-          path={`${match.path}`}
-          component={CollectionsOverviewContainer}
+            exact
+            path={`${match.path}`}
+            component={CollectionsOverviewContainer}
         />
         <Route
-          path={`${match.path}/:collectionId`}
-          component={CollectionPageContainer}
+            path={`${match.path}/:collectionId`}
+            component={CollectionPageContainer}
         />
-      </div>
+        </div>
     );
-  }
 }
 
 const mapDispatchToProps = dispatch => ({
